b477220e74193531803a2d699fefd38c36e6993b
quizandpuzzle <quizandpuzzle@gmail.com>
2023-07-24 20:26:50 +0300

    Implementing Player Attack

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private Transform _projectileSpawnPoint;
+
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Fire");
+            FireProjectile();
+        }
+    }
+
+    private void FireProjectile()
+    {
+
+        Instantiate(_projectile, _projectileSpawnPoint.position,
+            Quaternion.LookRotation(Vector3.forward, transform.up));
+
+    }
+}
